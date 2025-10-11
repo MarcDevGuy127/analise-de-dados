9d15322409b23671921b35ad2ee7f8778ca1f302
MarcDevGuy127 <marceloh.github@gmail.com>
2025-10-11 18:04:26 -0300

    Format column "nome"

--- EX1_Carga/limpeza.py
+++ EX1_Carga/limpeza.py
@@ -1,15 +1,21 @@
 import pandas as pd
+from unidecode import unidecode
+
 # Lendo um arquivo CSV
 df_csv = pd.read_csv('dados_clientes_sujos.csv')
-print(df_csv.head())
+#print(df_csv.head())
 
-dados = {
-    'Nome': ['Ana', 'Bruno', 'Carlos', 'Diana'],
-    'Idade': [25, 32, 29, 41],
-    'Salário': [2500, 3200, 2900, 4100]
-}
+df_csv['nome'] = df_csv['nome'].apply(unidecode)
 
-df = pd.DataFrame(dados)
+print(df_csv)
+
+# dados = {
+#    'Nome': ['Ana', 'Bruno', 'Carlos', 'Diana'],
+#    'Idade': [25, 32, 29, 41],
+#    'Salário': [2500, 3200, 2900, 4100]
+#}
+
+#df = pd.DataFrame(dados)
 
 # Visualizando os dados
-print(df)
+#print(df)
